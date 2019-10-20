883507f22168a48c17927126f79daa560cf0ad36
marcciszek <marciszek0@gmail.com>
2019-10-20 19:29:35 +0200

    Add files via upload

--- Lab_zad1/Lab_zad1/Lab_zad1.py
+++ Lab_zad1/Lab_zad1/Lab_zad1.py
@@ -2,61 +2,111 @@ import time     #biblioteka dzieki ktorej zatrzymujemy program na okreslony czas
 import os       #biblioteka pozwalajaca czyscic okno konsoli
 import math     #biblioteka zawierajaca liczbe PI
 
-dostepne_wybory = ('1','2','3','4') #pula wyborow dostepnych dla uzytkownika
-glowne_menu='''
+
+def Sprawdzenie(): #funkcja sprawdzajaca czy podana wartosc jest poprawna
+        while True:
+            try: #probujemy pobrac wartosc
+                wartosc = float(input(' >'))
+                break
+            except ValueError: #pomijamy wartosc ktora prowadzi do bledu programu
+                print('\n Wpisz poprawna liczbe!')
+        return wartosc #funkcja zwraca wartosc jesli jest ona poprawna
+
+def menu(): #funkcja w ktorej zawarte jest menu
+    dostepne_wybory = ('0','1','2','3','4') #pula wyborow dostepnych dla uzytkownika
+    glowne_menu='''
  Wybierz figure
  1.Kwadrat
  2.Prostokat
  3.Trojkat
  4.Kolo:
-'''
+ 0.Wyjscie
+    ''' # w ten sposob mozna zapisac pare linijek tekstu
+    print(glowne_menu)    
+    print(" Podaj odpowiadajaca cyfre")
+    while True:     #petla sprawdzajaca poprawnosc podanego wyboru
+        wybor_uzytkownika = input(' >')  #pobranie wyboru od uzytkownika
+        if wybor_uzytkownika in(dostepne_wybory):  #sprawdzenie czy wybor jest dostepny
+            return wybor_uzytkownika #zwrocenie wybory jesli znajduje sie na liscie
+        else:
+            os.system('cls') #wyczyszczenie konsoli
+            print(glowne_menu)
+            print(' Podaj poprawna cyfre')
 
-print(glowne_menu)    
-print(" Podaj odpowiadajaca cyfre")
+def koniec_programu(): #funkcja sprawdzajaca czy uzytkownik chce zakonczyc program
+   print("\n Czy chcesz zakonczyc program? T/N")
+   while True:          
+            wybor_uzytkownika = input(' >').upper() # .upper() zamienia male znaki na wielkie
+            if wybor_uzytkownika=='T':
+                return True
+            elif wybor_uzytkownika=='N':
+                return False
+            else:
+                os.system('cls')
+                print("\n Czy chcesz zakonczyc program? T/N")
+                print("\n Podaj poprawny znak")
 
-while True:     #petla sprawdzajaca poprawnosc podanego wyboru
-    wybor_uzytkownika = input(' >')  #pobranie wyboru od uzytkownika
-    if wybor_uzytkownika in(dostepne_wybory):  #sprawdzenie czy wybor jest dostepny
-        break
-    else:
-        os.system('cls')
-        print(glowne_menu)
-        print(' Podaj poprawna cyfre')
-
-os.system('cls') #czyszczenie konsoli
-
-if wybor_uzytkownika == '1':    #figura jest stringiem, dlatego wybory sa postaci 'x'
+def kwadrat():
     print("\n Wybrales kwadrat\n\n Podaj dlugosc boku")  #\n przechodzi do nowej linii
-    dl_bok = float(input(' >'))  #dlugosc boku pobierana jako liczba zmiennoprzecinkowa
+    dl_bok=Sprawdzenie()
     pole = dl_bok**2
     os.system('cls')
     print(f"\n Pole kwadratu {dl_bok} ^ 2 = {pole}")
-    
-if wybor_uzytkownika == '2':
+
+def prostokat():
     print("\n Wybrales prostokat\n\n Podaj dlugosc boku a")
-    dl_bok_a = float(input(' >'))
+    dl_bok_a=Sprawdzenie()
     print(" Podaj dlugosc boku b")
-    dl_bok_b = float(input(' >'))
+    dl_bok_b=Sprawdzenie()
     pole = dl_bok_a*dl_bok_b
     os.system('cls')
     print(f"\n Pole prostokatu {dl_bok_a} * {dl_bok_b} = {pole}")
 
-if wybor_uzytkownika == '3':
+def trojkat():
     print("\n Wybrales trojkat\n\n Podaj dlugosc podstawy")
-    dl_podstawy = float(input(' >'))
+    dl_podstawy = Sprawdzenie()
     print(" Podaj wysokosc")
-    wysokosc = float(input(' >'))
+    wysokosc = Sprawdzenie()
     pole = (dl_podstawy * wysokosc) / 2.0
     os.system('cls')
     print(f"\n Pole trojkata ({dl_podstawy} * {wysokosc}) / 2 = {pole}")
 
-
-if wybor_uzytkownika == '4':
+def kolo():
     print("\n Wybrales kolo\n\n Podaj dlugosc promienia")
-    promien = float(input(' >'))
+    promien = Sprawdzenie()
     pole = promien**2*math.pi
     os.system('cls')
     print(f"\n Pole kola {promien} ^ 2 * {round(math.pi,4)} = {round(pole,4)}")  #zaokraglenie(zmienna,ilosc miejsc po przecinku) 
-    print()
 
-time.sleep(10)
+
+
+while True: #Zapetla Dzialanie programu   
+    os.system('cls')
+    wybor_uzytkownika=menu() #wywolanie funkcji ktora zwroci wybor uzytkownika
+    os.system('cls')
+
+    if wybor_uzytkownika == '0': #wybor_uzytkownika jest stringiem, dlatego wybory sa zapisane jako znak, a nie wartosc
+        if koniec_programu()==True: 
+            break #jesli uzytkownik chce zakonczyc program to lamiemy petle while
+
+    elif wybor_uzytkownika == '1': #Inaczej else if
+        kwadrat()
+        if koniec_programu()==True:
+            break
+        
+    elif wybor_uzytkownika == '2': 
+        prostokat()
+        if koniec_programu()==True:
+            break
+
+    elif wybor_uzytkownika == '3':
+        trojkat()
+        if koniec_programu()==True:
+            break
+
+    elif wybor_uzytkownika == '4':
+        kolo()
+        if koniec_programu()==True:
+            break
+
+time.sleep(1)
